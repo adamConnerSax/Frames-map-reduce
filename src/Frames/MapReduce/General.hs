2ff58368ec175bd30d54e1420bcfc662ed2dbc22
adamConnerSax <adam_conner_sax@yahoo.com>
2019-09-18 11:34:46 -0400

    WIP. Have a compiling generalized MapReduce.  Next Folds, then make current Record and Rec (Maybe :. ElField) versions monomorphized versions of those?

--- src/Frames/MapReduce/General.hs
+++ src/Frames/MapReduce/General.hs
@@ -41,14 +41,14 @@ import qualified Data.Vinyl.Functor            as V
 import qualified Data.Vinyl.TypeLevel          as V
 import qualified Data.Vinyl.SRec               as V
 import qualified Data.Vinyl.ARec               as V
-
+import qualified Foreign.Storable              as FS
 
 {-
 rgetFieldG
   :: forall f t rs
    . (V.KnownField t, F.ElemOf rs t, V.FieldType (V.Fst t) rs ~ V.Snd t)
-  => (forall f rs. rt f 
-  -> rt (f :. ElField) rs
+  => (forall f rs. record f 
+  -> record (f :. ElField) rs
   -> f (V.Snd t)
 rgetFieldG = fmap V.getField . V.getCompose . V.rgetf (V.Label @(V.Fst t))
 -}
@@ -78,8 +78,8 @@ srecGetField
   -> f (V.Snd t)
 srecGetField = fmap V.getField . V.getCompose . V.sget @_ @t . V.getSRecNT --(V.Label @(V.Fst t))
 
-data RGetField t rt f where
-  RGetField :: (V.KnownField t, F.ElemOf rs t, Functor f) => (rt (f :. ElField) rs -> f (V.Snd t)) -> RGetField t rt f
+data RGetField t record f where
+  RGetField :: (V.KnownField t, F.ElemOf rs t, Functor f) => (record (f :. ElField) rs -> f (V.Snd t)) -> RGetField t record f
 
 recGetFieldF
   :: forall t f rs
@@ -103,12 +103,57 @@ srecGetFieldF
   => RGetField t V.SRec f
 srecGetFieldF = RGetField (srecGetField @t @f @rs)
 
-class RecGetFieldC t rt f where
-  rgetFieldF :: forall rs. ( V.KnownField t
-                           , Functor f
-                           , F.ElemOf rs t
-                           ) => rt (f :. ElField) rs -> f (V.Snd t)
+class RecGetFieldC t record f rs where
+  rgetFieldF :: ( V.KnownField t
+                , Functor f
+                , F.ElemOf rs t
+                ) => record (f :. ElField) rs -> f (V.Snd t)
 
+
+instance RecGetFieldC t V.Rec f rs where
+  rgetFieldF = recGetField @t
+
+instance RecGetFieldC t V.ARec f rs where
+  rgetFieldF = arecGetField @t
+
+instance (V.FieldOffset (f :. ElField) rs t) => RecGetFieldC t V.SRec f rs where
+  rgetFieldF = srecGetField @t @f @rs
+
+class RCastC rs ss record f  where
+  rcastF :: record (f :. ElField) ss -> record (f :. ElField) rs
+
+instance V.RecSubset V.Rec rs ss (V.RImage rs ss) => RCastC rs ss V.Rec f where
+  rcastF = V.rcast
+
+instance (V.IndexWitnesses (V.RImage rs ss), V.NatToInt (V.RLength rs)) => RCastC rs ss V.ARec f where
+  rcastF = V.arecGetSubset
+
+instance (V.RPureConstrained (V.FieldOffset (f :. ElField) ss) rs
+         , V.RPureConstrained (V.FieldOffset (f :. ElField) rs) rs
+         , V.RFoldMap rs
+         , V.RMap rs
+         , V.RApply rs
+         , FS.Storable (V.Rec (f :. ElField) rs)) =>  RCastC rs ss V.SRec f where
+  rcastF = V.SRecNT . V.srecGetSubset . V.getSRecNT
+
+class IsoRec rs record f where
+  toRec :: record (f :. ElField) rs -> V.Rec (f :. ElField) rs
+  fromRec :: V.Rec (f :. ElField) rs -> record (f :. ElField) rs
+
+instance IsoRec rs V.Rec f where
+  toRec = id
+  fromRec = id
+
+instance FS.Storable (V.Rec (f :. ElField) rs) => IsoRec rs V.SRec f where
+  toRec = V.fromSRec
+  fromRec = V.toSRec
+
+
+instance (V.NatToInt (V.RLength rs)
+         , V.RecApplicative rs
+         , V.RPureConstrained (V.IndexableField rs) rs) => IsoRec rs V.ARec f where
+  toRec = V.fromARec
+  fromRec = V.toARec
 {-
 type family RemoveIdentity (a :: Type -> Type) :: Type -> Type
 type instance RemoveIdentity (Identity :. ElField) = ElField
@@ -116,7 +161,7 @@ type instance RemoveIdentity a = a
 -}
 
 -- | This is only here so we can use hash maps for the grouping step.  This should properly be in Frames itself.
-instance Hash.Hashable (rt (f :. ElField)  '[]) where
+instance Hash.Hashable (record (f :. ElField)  '[]) where
   hash = const 0
   {-# INLINABLE hash #-}
   hashWithSalt s = const s -- TODO: this seems BAD! Or not?
@@ -124,124 +169,152 @@ instance Hash.Hashable (rt (f :. ElField)  '[]) where
 
 instance (V.KnownField t
          , Functor f
-         , RecGetFieldC t rt f
+         , RecGetFieldC t record f (t ': rs)
+         , RCastC rs (t ': rs) record f
          , Hash.Hashable (f (V.Snd t))
-         , Hash.Hashable (rt (f :. ElField) rs)
-         , rs F.⊆ (t ': rs)) => Hash.Hashable (rt (f :. ElField) (t ': rs)) where
-  hashWithSalt s r = s `Hash.hashWithSalt` (rgetFieldF @t r) `Hash.hashWithSalt` (F.rcast @rs r)
+         , Hash.Hashable (record (f :. ElField) rs)
+         ) => Hash.Hashable (record (f :. ElField) (t ': rs)) where
+  hashWithSalt s r = s `Hash.hashWithSalt` (rgetFieldF @t r) `Hash.hashWithSalt` (rcastF @rs r)
   {-# INLINABLE hashWithSalt #-}
-{-
+
 -- | Don't do anything 
-unpackNoOp
-  :: MR.Unpack (F.Rec (Maybe :. ElField) rs) (F.Rec (Maybe :. ElField) rs)
+unpackNoOp :: MR.Unpack (record (f :. ElField) rs) (record (f :. ElField) rs)
 unpackNoOp = MR.Filter (const True)
 
 -- | Filter records using a function on the entire record. 
 unpackFilterRow
-  :: (F.Rec (Maybe :. ElField) rs -> Bool)
-  -> MR.Unpack (F.Rec (Maybe :. ElField) rs) (F.Rec (Maybe :. ElField) rs)
+  :: (record (f :. ElField) rs -> Bool)
+  -> MR.Unpack (record (f :. ElField) rs) (record (f :. ElField) rs)
 unpackFilterRow test = MR.Filter test
 
 -- | Filter records based on a condition on only one field in the row.  Will usually require a Type Application to indicate which field.
 unpackFilterOnField
-  :: forall t rs
-   . (V.KnownField t, F.ElemOf rs t, V.FieldType (V.Fst t) rs ~ V.Snd t)
-  => (Maybe (V.Snd t) -> Bool)
-  -> MR.Unpack (F.Rec (Maybe :. ElField) rs) (F.Rec (Maybe :. ElField) rs)
-unpackFilterOnField test = unpackFilterRow (test . rgetMaybeField @t)
+  :: forall t rs record f
+   . (Functor f, V.KnownField t, F.ElemOf rs t, RecGetFieldC t record f rs)
+  => (f (V.Snd t) -> Bool)
+  -> MR.Unpack (record (f :. ElField) rs) (record (f :. ElField) rs)
+unpackFilterOnField test = unpackFilterRow (test . rgetFieldF @t)
 
 -- | An unpack step which specifies a subset of columns, cs,
--- (via a type-application) and then filters a @Rec (Maybe :. Elfield) rs@
+-- (via a type-application) and then filters a @record (Maybe :. Elfield) rs@
 -- to only rows which have all good data in that subset.
 unpackGoodRows
-  :: forall cs rs
-   . (cs F.⊆ rs)
-  => MR.Unpack (F.Rec (Maybe :. ElField) rs) (F.Rec (Maybe :. ElField) rs)
-unpackGoodRows = unpackFilterRow (isJust . F.recMaybe . F.rcast @cs)
+  :: forall cs rs record f
+   . (RCastC cs rs record f)
+  => (record (f :. ElField) cs -> Bool)
+  -> MR.Unpack (record (f :. ElField) rs) (record (f :. ElField) rs)
+unpackGoodRows testSubset = unpackFilterRow (testSubset . rcastF @cs)
 
 -- | Assign both keys and data cols.  Uses type applications to specify them if they cannot be inferred.
 -- Keys usually can't. Data sometimes can.
 assignKeysAndData
-  :: forall ks cs rs
-   . (ks F.⊆ rs, cs F.⊆ rs)
+  :: forall ks cs rs record f
+   . (RCastC ks rs record f, RCastC cs rs record f)
   => MR.Assign
-       (F.Rec (Maybe :. ElField) ks)
-       (F.Rec (Maybe :. ElField) rs)
-       (F.Rec (Maybe :. ElField) cs)
-assignKeysAndData = MR.assign (F.rcast @ks) (F.rcast @cs)
+       (record (f :. ElField) ks)
+       (record (f :. ElField) rs)
+       (record (f :. ElField) cs)
+assignKeysAndData = MR.assign (rcastF @ks) (rcastF @cs)
 {-# INLINABLE assignKeysAndData #-}
 
 -- | Assign keys and leave the rest of the columns, excluding the keys, in the data passed to reduce.
 splitOnKeys
-  :: forall ks rs cs
-   . (ks F.⊆ rs, cs ~ F.RDeleteAll ks rs, cs F.⊆ rs)
+  :: forall ks rs cs record f
+   . (RCastC ks rs record f, RCastC cs rs record f, cs ~ F.RDeleteAll ks rs)
   => MR.Assign
-       (F.Rec (Maybe :. ElField) ks)
-       (F.Rec (Maybe :. ElField) rs)
-       (F.Rec (Maybe :. ElField) cs)
+       (record (f :. ElField) ks)
+       (record (f :. ElField) rs)
+       (record (f :. ElField) cs)
 splitOnKeys = assignKeysAndData @ks @cs
 {-# INLINABLE splitOnKeys #-}
 
 -- | Assign keys and leave all columns, including the keys, in the data passed to reduce.
 assignKeys
-  :: forall ks rs
-   . (ks F.⊆ rs)
+  :: forall ks rs record f
+   . (RCastC ks rs record f)
   => MR.Assign
-       (F.Rec (Maybe :. ElField) ks)
-       (F.Rec (Maybe :. ElField) rs)
-       (F.Rec (Maybe :. ElField) rs)
-assignKeys = MR.assign (F.rcast @ks) id
+       (record (f :. ElField) ks)
+       (record (f :. ElField) rs)
+       (record (f :. ElField) rs)
+assignKeys = MR.assign (rcastF @ks) id
 {-# INLINABLE assignKeys #-}
 
 -- | Reduce the data to a single row and then re-attach the key.
+-- | NB: for all but Rec case, this will have to convert record to Rec and back for the append
 reduceAndAddKey
-  :: forall ks cs x
-   . FI.RecVec ((ks V.++ cs))
-  => (forall h . Foldable h => h x -> F.Rec (Maybe :. ElField) cs) -- ^ reduction step
+  :: forall ks cs x record f
+   . ( IsoRec ks record f
+     , IsoRec cs record f
+     , IsoRec (ks V.++ cs) record f
+     , FI.RecVec ((ks V.++ cs))
+     )
+  => (forall h . Foldable h => h x -> record (f :. ElField) cs) -- ^ reduction step
   -> MR.Reduce
-       (F.Rec (Maybe :. ElField) ks)
+       (record (f :. ElField) ks)
        x
-       (F.Rec (Maybe :. ElField) (ks V.++ cs))
-reduceAndAddKey process = MR.processAndLabel process V.rappend
+       (record (f :. ElField) (ks V.++ cs))
+reduceAndAddKey process =
+  MR.processAndLabel process (\k y -> fromRec (toRec k `V.rappend` toRec y))
 {-# INLINABLE reduceAndAddKey #-}
 
 -- | Reduce by folding the data to a single row and then re-attaching the key.
 foldAndAddKey
-  :: (FI.RecVec ((ks V.++ cs)))
+  :: ( IsoRec ks record f
+     , IsoRec cs record f
+     , IsoRec (ks V.++ cs) record f
+     , FI.RecVec ((ks V.++ cs))
+     )
   => FL.Fold x (F.Rec (Maybe :. ElField) cs) -- ^ reduction fold
   -> MR.Reduce
        (F.Rec (Maybe :. ElField) ks)
        x
        (F.Rec (Maybe :. ElField) (ks V.++ cs))
-foldAndAddKey fld = MR.foldAndLabel fld V.rappend
+foldAndAddKey fld =
+  MR.foldAndLabel fld (\k y -> fromRec (toRec k `V.rappend` toRec y))
 {-# INLINABLE foldAndAddKey #-}
 
 -- | Transform a reduce which produces a container of results, with a function from each result to a record,
 -- into a reduce which produces a FrameRec of the result records with the key re-attached.
 makeRecsWithKey
-  :: (Functor g, Foldable g, (FI.RecVec (ks V.++ as)))
-  => (y -> F.Rec (Maybe :. ElField) as) -- ^ map a result to a record
-  -> MR.Reduce (F.Rec (Maybe :. ElField) ks) x (g y) -- ^ original reduce
+  :: ( Functor g
+     , Foldable g
+     , IsoRec ks record f
+     , IsoRec as record f
+     , IsoRec (ks V.++ as) record f
+     , (FI.RecVec (ks V.++ as))
+     )
+  => (y -> record (f :. ElField) as) -- ^ map a result to a record
+  -> MR.Reduce (record (f :. ElField) ks) x (g y) -- ^ original reduce
   -> MR.Reduce
-       (F.Rec (Maybe :. ElField) ks)
+       (record (f :. ElField) ks)
        x
-       (g (F.Rec (Maybe :. ElField) (ks V.++ as)))
+       (g (record (f :. ElField) (ks V.++ as)))
 makeRecsWithKey makeRec reduceToY = MR.reduceMapWithKey addKey reduceToY
-  where addKey k = fmap (V.rappend k . makeRec)
+ where
+  addKey k = fmap (\y -> fromRec . V.rappend (toRec k) . toRec $ makeRec y)
 {-# INLINABLE makeRecsWithKey #-}
 
 -- | Transform an effectful reduce which produces a container of results, with a function from each result to a record,
 -- into a reduce which produces a FrameRec of the result records with the key re-attached.
 makeRecsWithKeyM
-  :: (Monad m, Functor g, Foldable g, (FI.RecVec (ks V.++ as)))
-  => (y -> F.Rec (Maybe :. ElField) as) -- ^ map a result to a record
-  -> MR.ReduceM m (F.Rec (Maybe :. ElField) ks) x (g y) -- ^ original reduce
+  :: ( Monad m
+     , Functor g
+     , Foldable g
+     , IsoRec ks record f
+     , IsoRec as record f
+     , IsoRec (ks V.++ as) record f
+     , (FI.RecVec (ks V.++ as))
+     )
+  => (y -> record (f :. ElField) as) -- ^ map a result to a record
+  -> MR.ReduceM m (record (f :. ElField) ks) x (g y) -- ^ original reduce
   -> MR.ReduceM
        m
-       (F.Rec (Maybe :. ElField) ks)
+       (record (f :. ElField) ks)
        x
-       (g (F.Rec (Maybe :. ElField) (ks V.++ as)))
+       (g (record (f :. ElField) (ks V.++ as)))
 makeRecsWithKeyM makeRec reduceToY = MR.reduceMMapWithKey addKey reduceToY
-  where addKey k = fmap (V.rappend k . makeRec)
+ where
+  addKey k = fmap (\y -> fromRec . V.rappend (toRec k) . toRec $ makeRec y)
 {-# INLINABLE makeRecsWithKeyM #-}
--}
+
+
